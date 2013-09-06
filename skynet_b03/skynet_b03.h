a46f2357d71593fb7d5a863ac887163cb01cdc21
Sean Hara <smyh777@hawaii.edu>
2013-09-05 20:44:41 -1000

    Weatherbox: Added comments to skynet_b03.h

--- skynet_b03/skynet_b03.h
+++ skynet_b03/skynet_b03.h
@@ -6,21 +6,7 @@
  *          The main driver header file for the REIS weatherbox. 
  *
  ****************************************************/
-#include <Wire.h>
-#include <Adafruit_BMP085.h>
-#include <SoftwareSerial.h>
-#include <XBee.h>
-#include <Adafruit_INA219.h>
 
-#include <EEPROM.h>
-
-#include <avr/sleep.h>
-#include <avr/power.h>
-#include <avr/wdt.h>
-
-#include <SHT1x.h>
-#include <OneWire.h>
-#include <DallasTemperature.h>
 
 long sampleBatteryVoltage(void);
 long sampleSmoothBatteryV(int sample);
