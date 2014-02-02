05fe0a75bfb65fab7db33d4585130b6d65721686
Sean Hara <smyh777@hawaii.edu>
2014-02-01 16:32:51 -1000

    xbeetxrx: Added my receive and transmit testing code

---
+++ test_benches/xbeeTxRx/rxSH/rxSH.ino
@@ -0,0 +1,49 @@
+/*
+A2AExample_Receiver.pde
+
+This program keep listening to incoming messages, and print received 
+messages to serial port.
+*/
+#include <XBee.h>
+
+XBee xbee = XBee();
+
+// response object to handle incomming messages 
+ZBRxResponse ZBrx = ZBRxResponse();
+  
+void setup() {
+  Serial.begin(9600);
+  xbee.begin(Serial);
+}
+
+void loop() 
+{
+  // Start reading
+  xbee.readPacket();
+
+  // Checks if there is a packet
+  if (xbee.getResponse().isAvailable()) 
+  {
+    // Checks if the packet is a rx packet
+    if (xbee.getResponse().getApiId() == ZB_RX_RESPONSE) 
+    {
+      // Reads and displays values
+      xbee.getResponse().getZBRxResponse(ZBrx);
+      for (int i = 0; i < ZBrx.getDataLength (); i++)
+        Serial.println(ZBrx.getData(i));
+
+      // Displays size of packet received
+      Serial.print("\nSize of Packet: ");
+      Serial.println(ZBrx.getDataLength());
+      Serial.println();
+      }
+    }
+  }
+}
+
+
+
+
+
+
+
---
+++ test_benches/xbeeTxRx/txSH/txSH.ino
@@ -0,0 +1,53 @@
+
+#include <XBee.h>
+
+#include "schema.h"
+
+XBee xbee = XBee();
+XBeeAddress64 addr64 = XBeeAddress64(0, 0);
+
+uint8_t payload[84];
+
+schema_1 packet;
+
+void setup()
+{
+  Serial.begin(9600);
+  xbee.begin(Serial);
+}
+
+void loop()
+{
+  /*packet.schema = 1;
+  packet.address = 0;		// Address of Arduino	
+  packet.uptime_ms = 0;		// Time since start of program
+  packet.n = 0;			// number of data points in packet 0..30 
+  for(int count = 0; count < 7; count++)
+  {
+    packet.batt_mv[count] = 0;	// Battery Voltage (in milli volts)
+    packet.panel_mv[count] = 0;	// Panel Voltage (in milli volts)
+    packet.apogee_w_m2[count] = 0;
+  }
+  packet.bmp085_press_pa = 0;	// Pressure Value (in pascals)
+  packet.bmp085_temp_decic = 0;  // Temperature Value (in celsius)
+  packet.humidity_centi_pct = 0;
+  packet.gps_valid_sats = 0;
+  packet.gps_year = 0;
+  packet.gps_month = 0;
+  packet.gps_day = 0;
+  packet.gps_hour = 0;
+  packet.gps_minute = 0;
+  packet.gps_second = 0;
+  packet.gps_lat = 0;
+  packet.gps_lon = 0;
+  packet.gps_elev = 0;*/
+  
+  // Initialize packet to 0
+  memset(payload, '\0', sizeof(payload));
+  memcpy(payload, &packet, sizeof(packet));
+
+  // Transmit Packet
+  ZBTxRequest zbTx = ZBTxRequest(addr64, payload, sizeof(packet));
+  xbee.send(zbTx);
+  delay(1000);
+}
