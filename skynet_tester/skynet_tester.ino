a4d42445f42c5a6a1c720c52e0b59bd7cea189f2
Kenny Luong <luong97@hawaii.edu>
2014-03-01 20:12:19 -1000

    Added a tentitive skynet tester file

---
+++ skynet_tester/skynet_tester.ino
@@ -0,0 +1,19 @@
+/* 
+
+Our awesome skynet testbench program.. 
+
+Keep this file mainly empty. We want to abstract everything 
+we can outside of the arduino architecture, so when we move,
+it'll be nice and easy. 
+
+*/
+
+void setup(){
+
+}
+
+void loop(){
+}
+
+
+
