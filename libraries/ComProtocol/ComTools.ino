a12f91a28b392b820e27d14cbf30dc0a7524740b
Zachary Dorman <zeearedee@gmail.com>
2014-05-22 17:33:56 -1000

    Removed code irrelevant to com protocol.

--- libraries/ComProtocol/ComTools.ino
+++ libraries/ComProtocol/ComTools.ino
@@ -29,7 +29,10 @@ void initExpPk(expPk* val)
 
 void storeData(uint8_t *data, uint8_t length)
 {  
-      Serial.print('k');
+  for (int i = 3; i < length; i++)
+  {
+    Serial.println(data[i]);
+  }
 }
 
 
@@ -37,12 +40,11 @@ void storeData(uint8_t *data, uint8_t length)
 void storeWPData(uint8_t *data, uint8_t length)
 {  
     //store .hex fragment
-      for (int i = 3; i < length; i++)
-      { //hangs here if no EE connected
-        //writeEE(writeAddr, data[i]);      
-        
-        //incrementAddr(&writeAddr);
-      }
+    for (int i = 3; i < length; i++)
+    { //hangs here if no EE connected
+      writeEE(writeAddr, data[i]);      
+      incrementAddr(&writeAddr);
+    }
 }
 
 
@@ -57,6 +59,58 @@ void requestPacket(expPk val)
   ZBTxRequest zbTx = ZBTxRequest(addr64, payload, sizeof(payload));
   xbee.send(zbTx);
 }
+
+
+
+
+
+void handleXBeeRx()
+{
+  uint8_t header = 0;
+  
+  //fill in values
+  xbee.getResponse().getZBRxResponse(rx);
+  
+  //do something with data if this is an expected packet
+  if ( rx.getData(1) * 256 + rx.getData(2) == expectedPk.nextPk )
+  {
+    //set values if this is the first packet
+    if ( expectedPk.nextPk == 0 )
+    {
+      expectedPk.nextPk++;
+      expectedPk.header = rx.getData(0);
+      expectedPk.totalPk = (uint16_t)rx.getData(3) * 256 + rx.getData(4);
+    }
+    
+    else if ( rx.getData(0) == expectedPk.header )
+    {
+      if ( rx.getData(0) == _SOME_HEADER )
+        storeData(rx.getData(), rx.getDataLength());
+      
+      else if ( rx.getData(0) == _WP_HEADER )
+        storeWPData(rx.getData(), rx.getDataLength());
+        
+      expectedPk.nextPk++;
+    }
+  }
+  
+  //cover all cases for final packets
+  //set and reset designated values
+  if ( expectedPk.nextPk == expectedPk.totalPk + 1)
+  {
+    readAddr.final = (uint16_t)writeAddr.high * 256 + writeAddr.low - 1;
+    initAddr(&writeAddr);
+    initExpPk(&expectedPk);
+  }
+  
+  //request the next expected packet
+  requestPacket(expectedPk);
+    
+}
+
+
+
+
 
 
 void writeEE(eepromAddr val, uint8_t data)
@@ -79,19 +133,3 @@ uint8_t readEE(eepromAddr val)
  return Wire.read();
 }
 
-
-
-
-
-
-
-
-void print_data(eepromAddr val, uint8_t data)
-{
- Serial.print("\n"); 
- Serial.print(data, HEX);
- Serial.print(" Address   ");
- Serial.print(int(val.high));
- Serial.print("     ");
- Serial.println(int(val.low)); 
-}
